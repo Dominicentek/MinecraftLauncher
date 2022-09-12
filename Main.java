0fc7a3cc22f6a8dfe9984811ae1fc1a6c28a3399
Dominicentek <69892109+Dominicentek@users.noreply.github.com>
2022-09-12 10:36:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,7 +33,12 @@ public class Main {
         username.setBounds(138, 34, 128, 24);
         launch.setBounds(5, 63, 261, 32);
         launch.addActionListener(event -> {
-            launch(username.getText(), versions.getSelectedIndex());
+            try {
+                launch(username.getText(), versions.getSelectedIndex());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         });
         frame.add(versionLabel);
         frame.add(usernameLabel);
@@ -64,7 +69,7 @@ public class Main {
         }
     }
     public static void launch(String username, int index) throws Exception {
-        System.out.println("Launching version " + versionList.get(index) + "...")
+        System.out.println("Launching version " + versionList.get(index) + "...");
         String versionData = new String(download(jsonLinks.get(index)));
         JsonObject root = new JsonParser().parse(versionData).getAsJsonObject();
         String assetIndex = root.get("assetIndex").getAsJsonObject().get("id").getAsString();
@@ -73,27 +78,96 @@ public class Main {
         new File("mc/libraries").mkdir();
         new File("mc/assets").mkdir();
         new File("mc/assets/index").mkdir();
-        new File("mc/assets/data").mkdir();
+        new File("mc/assets/objects").mkdir();
         File client = new File("mc/versions/" + versionList.get(index) + ".jar");
-        File index = new File("mc/assets/index/" + assetIndex + ".json");
+        File indexFile = new File("mc/assets/index/" + assetIndex + ".json");
+        File libraries = new File("mc/libraries");
+        libraries.mkdir();
         if (!client.exists()) {
             System.out.println("Downloading client...");
             byte[] clientData = download(root.get("downloads").getAsJsonObject().get("client").getAsJsonObject().get("url").getAsString());
-            OutputStream out = new FileOutputStream(new File("mc/client.jar"));
+            OutputStream out = new FileOutputStream(new File("mc/versions/" + versionList.get(index) + ".jar"));
             out.write(clientData);
             out.close();
         }
         System.out.println("Downloading assets...");
         String assetIndexJsonData;
-        if (!index.exists()) {
+        if (!indexFile.exists()) {
             System.out.println("Downloading asset index...");
-            byte[] assetIndexData = download(root.get("assetIndex").getAsJsonObject().get("url"));
-            OutputStream out = new FileOutputStream(new File("mc/assets/index/" + assetIndex + ".json"));
+            byte[] assetIndexData = download(root.get("assetIndex").getAsJsonObject().get("url").getAsString());
+            OutputStream out = new FileOutputStream(indexFile);
             out.write(assetIndexData);
             out.close();
             assetIndexJsonData = new String(assetIndexData);
         }
         else {
+            InputStream in = new FileInputStream(indexFile);
+            byte[] assetIndexData = new byte[in.available()];
+            in.read(assetIndexData);
+            in.close();
+            assetIndexJsonData = new String(assetIndexData);
         }
+        JsonObject assetIndexJson = new JsonParser().parse(assetIndexJsonData).getAsJsonObject().get("objects").getAsJsonObject();
+        for (String key : assetIndexJson.keySet()) {
+            JsonObject assetJson = assetIndexJson.get(key).getAsJsonObject();
+            String hash = assetJson.get("hash").getAsString();
+            String firstTwo = hash.substring(0, 2);
+            File assetFile = new File("mc/assets/objects/" + firstTwo + "/" + hash);
+            if (!assetFile.exists()) {
+                System.out.println("Downloading asset " + key + "...");
+                assetFile.getParentFile().mkdirs();
+                OutputStream out = new FileOutputStream(assetFile);
+                out.write(download("http://resources.download.minecraft.net/" + firstTwo + "/" + hash));
+                out.close();
+            }
+        }
+        System.out.println("Downloading libraries...");
+        for (JsonElement libraryElement : root.get("libraries").getAsJsonArray()) {
+            JsonObject libraryJson = libraryElement.getAsJsonObject().get("downloads").getAsJsonObject().get("artifact").getAsJsonObject();
+            String path = libraryJson.get("path").getAsString();
+            File libraryFile = new File(libraries, path);
+            if (!libraryFile.exists()) {
+                System.out.println("Downloading library " + path + "...");
+                libraryFile.getParentFile().mkdirs();
+                OutputStream out = new FileOutputStream(libraryFile);
+                out.write(download(libraryJson.get("url").getAsString()));
+                out.close();
+            }
+        }
+        System.out.println("Starting Minecraft...");
+        String[] args = new String[] {
+            "java/bin/java.exe",
+            "-cp",
+            root.get("mainClass").getAsString(),
+            "mc/versions/" + versionList.get(index) + ".jar",
+            "--username",
+            username,
+            "--gameDir",
+            "./mc",
+            "--assetsDir",
+            "./mc/assets",
+            "--assetIndex",
+            assetIndex,
+            "--accessToken",
+            "0"
+        };
+        Process minecraft = new ProcessBuilder().command(args).start();
+        InputStream in = minecraft.getInputStream();
+        Thread inThread = new Thread(() -> {
+            try {
+                while (true) {
+                    while (in.available() > 0) {
+                        System.out.print((char)in.read());
+                    }
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        inThread.start();
+        minecraft.waitFor();
+        inThread.interrupt();
+        System.out.println("Minecraft ended");
     }
 }
